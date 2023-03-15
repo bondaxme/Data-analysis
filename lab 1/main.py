78daf6b28f95cd451e2afa763953b1afef3f8d47
bondaxme <maksbond280104@gmail.com>
2023-03-16 01:05:28 +0200

    data formatting completed

--- lab 1/main.py
+++ lab 1/main.py
@@ -1,43 +1,49 @@
 import csv
 from datetime import datetime
 
+def format_date(row_name, source, dest, start_date_format, flag):
+    with open(source, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
 
-def format_date():
+        with open(dest, 'w', newline='') as outfile:
+            fieldnames = reader.fieldnames
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in reader:
+                date_str = row[row_name]
+                date = datetime.strptime(date_str, start_date_format)
+                updated_date_str = date.strftime('%Y-%m-%d')
+                row[row_name] = updated_date_str
+                if flag == 1:
+                    old_value = row['match_id']
+                    new_value = old_value[2:]
+                    row['match_id'] = new_value
+                writer.writerow(row)
+
+def format_stadiums_names():
     with open('matches.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
+        rows = list(reader)
 
-        with open('updated_matches.csv', 'w', newline='') as outfile:
-            fieldnames = reader.fieldnames
-            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-            writer.writeheader()
+    for row in rows:
+        if row['home_team'] == 'RB Leipzig' and row['stadium'] == 'Red Bull Arena':
+            row['stadium'] = 'Red Bull Arena (Leipzig)'
+        elif row['home_team'] == 'RB Salzburg' and row['stadium'] == 'Red Bull Arena':
+            row['stadium'] = 'Red Bull Arena (Salzburg)'
 
-            for row in reader:
-                date_str = row['date_time']
-                date = datetime.strptime(date_str, '%d-%b-%y %I.%M.%S.%f000000 PM')
-                updated_date_str = date.strftime('%Y-%m-%d %H:%M:%S')
-                row['date_time'] = updated_date_str
-                old_value = row['match_id']
-                new_value = old_value[2:]
-                row['match_id'] = new_value
-                writer.writerow(row)
+    with open('matches.csv', 'w', newline='') as csvfile:
+        fieldnames = ['id', 'season', 'date_time', 'home_team', 'away_team', 'stadium', 'home_team_score',
+                      'away_team_score', 'penalty_shoot_out', 'attendance']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-def format_date_managers():
-    with open('managers.csv', 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
 
-        with open('updated_managers.csv', 'w', newline='') as outfile:
-            fieldnames = reader.fieldnames
-            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-            writer.writeheader()
 
-            for row in reader:
-                date_str = row['date_of_birth']
-                date = datetime.strptime(date_str, '%m-%d-%Y')
-                updated_date_str = date.strftime('%Y-%m-%d')
-                row['date_of_birth'] = updated_date_str
-                writer.writerow(row)
-
-format_date_managers()
+format_date('date_of_birth', 'managers.csv', 'updated_managers.csv', '%m-%d-%Y', 0)
+format_date('date_time', 'matches.csv', 'updated_matches.csv', '%d-%b-%y %I.%M.%S.%f000000 PM', 1)
+format_stadiums_names()
 
 
 
